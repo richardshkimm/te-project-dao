ad3e8391747f2d677a3ff5c7f2b8e048dbb4a580
wjjiang1 <wjj26@cornell.edu>
2023-04-27 23:51:09 -0400

    model

--- main.py
+++ main.py
@@ -8,20 +8,19 @@ from gurobipy import GRB
 
 def parse_topology(file_path):
     graph = nx.DiGraph()
-    with open(file_path, 'r') as f:
+    with open(file_path, "r") as f:
         lines = f.readlines()
         for line in lines:
-            if line.startswith('to_node'):
+            if line.startswith("to_node"):
                 continue
             to_node, from_node, capacity, prob_failure = line.strip().split()
-            graph.add_edge(int(to_node), int(from_node),
-                           capacity=int(capacity))
+            graph.add_edge(int(to_node), int(from_node), capacity=int(capacity))
     return graph
 
 
 def parse_demands(file_path, num_nodes):
     demands = {}
-    with open(file_path, 'r') as f:
+    with open(file_path, "r") as f:
         lines = f.readlines()
         for line in lines:
             demand_values = line.strip().split()
@@ -29,49 +28,53 @@ def parse_demands(file_path, num_nodes):
                 for j in range(num_nodes):
                     demand = float(demand_values[i * num_nodes + j])
                     if i != j:
-                        demands[(i+1, j+1)] = demand
+                        demands[(i + 1, j + 1)] = demand
     return demands
 
 
 def draw(graph):
     pos = nx.circular_layout(graph)
-    nx.draw(graph, pos, with_labels=True, node_size=800, node_color='lightblue',
-            edge_color='gray', font_size=12, font_weight='bold')
+    nx.draw(
+        graph,
+        pos,
+        with_labels=True,
+        node_size=800,
+        node_color="lightblue",
+        edge_color="gray",
+        font_size=12,
+        font_weight="bold",
+    )
 
-    labels = nx.get_edge_attributes(graph, 'capacity')
+    labels = nx.get_edge_attributes(graph, "capacity")
     for key in labels:
-        labels[key] = f'{labels[key]} Gbps'
+        labels[key] = f"{labels[key]} Gbps"
     nx.draw_networkx_edge_labels(graph, pos, font_size=8, edge_labels=labels)
     plt.show()
 
 
 def create_mcf_model(graph, demands):
-    edge_capacity_dict = {
-        (u, v): c for u, v, c in graph.edges(data='capacity')}
+    edge_capacity_dict = {(u, v): c for u, v, c in graph.edges(data="capacity")}
     edges, capacity = gp.multidict(edge_capacity_dict)
 
-    T = {(s, d): list(nx.all_simple_paths(graph, s, d)) for s, d in demands}
+    T = {}
+    for s, d in demands:
+        tunnels = nx.all_simple_paths(graph, s, d)
+        T[s, d] = []
+        for tunnel in tunnels:
+            tunnel_edges = list(zip(tunnel, tunnel[1:]))
+            T[s, d].append(tunnel_edges)
 
     model = gp.Model("MCF")
 
     # Variables
     flow = model.addVars(edges, name="FLOW")
-    # flow = {}
-    # for i, j, d in demands:
-    #     for u, v, c in graph.edges(data='capacity'):
-    #         flow[u, v, i, j] = model.addVar(
-    #             vtype=GRB.continuous, name=f'flow_{u}_{v}_{i}_{j}')
 
     # Constraints
-    model.addConstr(
-        (flow.sum(i, j) <= capacity[i, j] for i, j in edges), "CAPACITY")
-
-    model.addConstr((flow.sum(T[d]) <= demands[d]
-                    for d in demands), "DEMANDS")
+    model.addConstrs((flow.sum(i, j) <= capacity[i, j] for i, j in edges), "CAPACITY")
+    model.addConstrs((flow.sum(T[d]) <= demands[d] for d in demands), "DEMANDS")
 
     # Objective
-    throughput = flow.sum(d for d in demands)
-    model.setObjective(throughput, GRB.MAXIMIZE)
+    model.setObjective(flow.sum(), GRB.MAXIMIZE)
 
     # Optimize
     model.optimize()
@@ -84,8 +87,8 @@ def draw_flow_allocations(graph, demands, flow_vars):
 
 
 def main():
-    topology_file = 'topology.txt'
-    demands_file = 'demand.txt'
+    topology_file = "topology.txt"
+    demands_file = "demand.txt"
 
     graph = parse_topology(topology_file)
     num_nodes = graph.number_of_nodes()
@@ -94,12 +97,12 @@ def main():
     # draw(graph)
     create_mcf_model(graph, demands)
 
-    # mcf_model, flow_vars = create_mcf_model(graph, demands)
-    # if mcf_model.status == GRB.OPTIMAL:
-    #     print("Total throughput:", mcf_model.objVal, "Gbps")
-    #     # draw_flow_allocations(graph, demands, flow_vars)
-    # else:
-    #     print("No optimal solution found.")
+    mcf_model, flow_vars = create_mcf_model(graph, demands)
+    if mcf_model.status == GRB.OPTIMAL:
+        print("Total throughput:", mcf_model.objVal, "Gbps")
+        # draw_flow_allocations(graph, demands, flow_vars)
+    else:
+        print("No optimal solution found.")
 
 
 if __name__ == "__main__":
