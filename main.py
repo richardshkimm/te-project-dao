cb9730c020672b5fa5d03cfa00bcfdffd07000bf
rk625@cornell.edu <richardshkim04@gmail.com>
2023-05-02 15:54:36 -0400

    Add comments and implement visualizations with graphviz (Task 5)

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import gurobipy as gp
 from gurobipy import GRB
 from collections import defaultdict
+import pygraphviz as pgv
+
 # B4 Topology
 
 
@@ -48,13 +50,11 @@ def draw(graph):
 def create_mcf_model(graph, demands):
     edge_capacity_dict = {
         (u, v): c for u, v, c in graph.edges(data='capacity')}
-
     edges, capacity = gp.multidict(edge_capacity_dict)
-
-    paths = {(s, d): list(nx.all_simple_edge_paths(graph, s, d)) for s, d in demands}
+    paths = {(s, d): list(nx.all_simple_edge_paths(graph, s, d))
+             for s, d in demands}
 
     flow = {}
-
     model = gp.Model("mcf")
 
     edges_to_flow = defaultdict(list)
@@ -64,6 +64,7 @@ def create_mcf_model(graph, demands):
     enum_sd = []
     iter_sd = []
 
+    # create flow variables + update dicts/lists
     for s, d in paths:
         for i, path in enumerate(paths[s, d]):
             enum_sd.append((s, d, i))
@@ -71,51 +72,89 @@ def create_mcf_model(graph, demands):
                 if (s, d, i) not in rep_sd:
                     rep_sd[s, d, i] = a, b
                     iter_sd.append((s, d, a, b, i))
-                flow[s, d, a, b, i] = model.addVar(vtype=GRB.CONTINUOUS, name=f'flow_{s}_{d}_{a}_{b}_{i}')
+                flow[s, d, a, b, i] = model.addVar(
+                    vtype=GRB.CONTINUOUS, name=f'flow_{s}_{d}_{a}_{b}_{i}')
                 sd_to_flow[s, d].append((a, b, i))
                 edges_to_flow[a, b].append((s, d, i))
                 rep_vals[s, d, i].append((a, b))
 
-
+    # Flow Constraints
     for s, d, i in enum_sd:
         for a1, b1 in rep_vals[s, d, i]:
             for a2, b2 in rep_vals[s, d, i]:
-                model.addConstr(flow[s, d, a1, b1, i] == flow[s, d, a2, b2, i], f'flow_{s}_{d}_{a1}_{b1}_{a2}_{b2}_{i}')
+                model.addConstr(flow[s, d, a1, b1, i] == flow[s, d,
+                                a2, b2, i], f'flow_{s}_{d}_{a1}_{b1}_{a2}_{b2}_{i}')
 
-    model.addConstrs(((gp.quicksum(flow[s, d, a, b, i] for s, d, i in edges_to_flow[a, b])) <= capacity[a, b] for a, b in edges), "CAPACITY")
+    # Capacity constraints
+    model.addConstrs(((gp.quicksum(
+        flow[s, d, a, b, i] for s, d, i in edges_to_flow[a, b])) <= capacity[a, b] for a, b in edges), "CAPACITY")
 
-    mlu= model.addVar(vtype=GRB.CONTINUOUS, name="MLU")
+    # MLU Capacity constraints
+    mlu = model.addVar(vtype=GRB.CONTINUOUS, name="MLU")
+    model.addConstrs(((gp.quicksum(
+        flow[s, d, a, b, i] for s, d, i in edges_to_flow[a, b])) <= mlu for a, b in edges), "MLU CAP")
 
-    model.addConstrs(((gp.quicksum(flow[s, d, a, b, i] for s, d, i in edges_to_flow[a, b])) <= mlu for a, b in edges), "CAPACITY")
-
+    # "Demand" constraints
     for s, d in demands:
-        model.addConstr(gp.quicksum(flow[s, d, rep_sd[s, d, i][0], rep_sd[s, d, i][1], i] for i in range(len(paths[s, d]))) <= demands[s, d], f"DEMAND_{s}_{d}")
+        model.addConstr(gp.quicksum(flow[s, d, rep_sd[s, d, i][0], rep_sd[s, d, i][1], i] for i in range(
+            len(paths[s, d]))) <= demands[s, d], f"DEMAND_{s}_{d}")
 
-    c = 0 #This is for part 5
-    # c = 1000
-    model.setObjective(gp.quicksum(flow[s, d, a, b, i] for s, d, a, b, i in iter_sd) - mlu * c, GRB.MAXIMIZE)
+    c = 0  # How important minimizing MLU is for optimization
+    model.setObjective(gp.quicksum(
+        flow[s, d, a, b, i] for s, d, a, b, i in iter_sd) - mlu * c, GRB.MAXIMIZE)
 
     model.optimize()
 
-    for v in model.getVars():
-        if v.X != 0:
-            print('%s %g' % (v.VarName, v.X))
+    # Testing
+    # for v in model.getVars():
+    #     if v.X != 0:
+    #         print('%s %g' % (v.VarName, v.X))
+
+    return model, flow
 
 
-def draw_flow_allocations(graph, demands, flow_vars):
-    return
+def draw_flow_allocations(graph, flow):
+    # Adding flow
+    for s, d, a, b, i in flow:
+        if flow[s, d, a, b, i].x > 0:
+            if graph.has_edge(a, b):
+                if 'flow' in graph[a][b]:
+                    graph[a][b]['flow'] += flow[s, d, a, b, i].x
+                else:
+                    graph[a][b]['flow'] = flow[s, d, a, b, i].x
+            else:
+                graph.add_edge(a, b, flow=flow[s, d, a, b, i].x)
+
+    # Convert graph (networkx --> pygraphviz)
+    A = nx.nx_agraph.to_agraph(graph)
+
+    # Appearance
+    A.node_attr.update(color="lightblue", style="filled", fontsize=14)
+    A.edge_attr.update(fontsize=10)
+
+    # Flow labels
+    for a, b in A.edges():
+        flow_value = graph[int(a)][int(b)]['flow']
+        A.get_edge(a, b).attr['label'] = f"{flow_value:.2f}"
+
+    # Render + Display
+    A.draw("flow_allocations.png", prog='dot')
+    A.view("flow_allocations.png")
 
 
 def main():
     topology_file = 'topology.txt'
     demands_file = 'demand.txt'
 
-    graph = parse_topology(topology_file)
-    num_nodes = graph.number_of_nodes()
+    graph1 = parse_topology(topology_file)
+    num_nodes = graph1.number_of_nodes()
     demands = parse_demands(demands_file, num_nodes)
     # print(demands)
     # draw(graph)
-    create_mcf_model(graph, demands)
+    graph2 = graph1.copy()
+    mcf_model, flow = create_mcf_model(graph1, demands)
+    print("Total throughput:", mcf_model.objVal, "Gbps")
+    draw_flow_allocations(graph2, flow)
 
     # mcf_model, flow_vars = create_mcf_model(graph, demands)
     # if mcf_model.status == GRB.OPTIMAL:
