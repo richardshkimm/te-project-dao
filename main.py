a7ba5bfe16d78d643b0c0fb01a807bd4781f3bed
Grace Ge <gg398@cornell.edu>
2023-05-09 19:11:24 -0400

    Simplify constraints

--- main.py
+++ main.py
@@ -71,7 +71,6 @@ def draw(graph):
 
 # mlu_weight is how important minimizing MLU is for optimization
 def create_mcf_model(graph, demands, mlu_weight = 0):
-    print(enumerate(demands.keys()))
     total_demand = sum(demands.values())
     edge_capacity_dict = {
         (u, v): c for u, v, c in graph.edges(data='capacity')}
@@ -79,83 +78,63 @@ def create_mcf_model(graph, demands, mlu_weight = 0):
     
     paths = defaultdict(list)
     for s, d in demands:
-        for x in list(islice(nx.shortest_simple_paths(graph, s, d), 10)):
-            if s != d:
-                paths[s, d].append(list(combinations(x, 2)))
+        paths = {(s, d): list(nx.all_simple_edge_paths(graph, s, d)) for s, d in demands}
+        # for x in list(islice(nx.shortest_simple_paths(graph, s, d), 10)):
+        #     if s != d:
+        #         paths[s, d].append(list(combinations(x, 2)))
+
     flow = {}
     model = gp.Model("mcf")
 
     # Maps edges to flow variables
     edges_to_flow = defaultdict(list)
-
-    # List of (source, destination, and id) for each path between every source and destination
-    iter_sd = []
-
-    # Create flow variables for each edge within each path between source and destination
     for s, d in paths:
         for idx, path in enumerate(paths[s, d]):
-            iter_sd.append((s, d, idx))
             for i, j in path:
-                flow[s, d, i, j, idx] = model.addVar(
-                    vtype=GRB.CONTINUOUS, name=f'flow_{s}_{d}_{i}_{j}_{idx}')
                 edges_to_flow[i, j].append((s, d, idx))
-
-    # Conservation Constraints
-    for s, d in paths:
-        for idx, path in enumerate(paths[s, d]):
-            # Can be made more efficient by only comparing with one right before it
-            for i in range(1, len(path)):
-                i1, j1 = path[i - 1][0], path[i - 1][1]
-                i2, j2 = path[i][0], path[i][1]
-                model.addConstr(flow[s, d, i1, j1, idx] == flow[s, d, i2, j2, idx], f'flow_{s}_{d}_{i1}_{j1}_{i2}_{j2}_{idx}')
-
-    # Capacity constraints
-    model.addConstrs((((gp.quicksum(
-        flow[s, d, i, j, idx] for s, d, idx in edges_to_flow[i, j])) + (gp.quicksum(
-        flow[s, d, j, i, idx] for s, d, idx in edges_to_flow[j, i]))) <= capacity[i, j] for i, j in edges), "CAPACITY")
+            flow[s, d, idx] = model.addVar(
+                vtype=GRB.CONTINUOUS, name=f'flow_{s}_{d}_{idx}')
+    
+    # List of (source, destination, and id) for each path between every source and destination
+    model.addConstrs(((gp.quicksum(flow[s, d, idx] for s, d, idx in edges_to_flow[i, j])) + (gp.quicksum(
+        flow[s, d, idx] for s, d, idx in edges_to_flow[j, i])) <= capacity[i, j] for i, j in edges), "CAPACITY")
 
     # MLU Capacity constraints
     mlu = model.addVar(vtype=GRB.CONTINUOUS, name="MLU")
-    model.addConstrs((((gp.quicksum(
-        flow[s, d, i, j, idx] for s, d, idx in edges_to_flow[i, j])) + (gp.quicksum(
-        flow[s, d, j, i, idx] for s, d, idx in edges_to_flow[j, i])))/capacity[i, j] <= mlu for i, j in edges), "MLU CAP")
+    model.addConstrs(((((gp.quicksum(flow[s, d, idx] for s, d, idx in edges_to_flow[i, j])) + (gp.quicksum(
+        flow[s, d, idx] for s, d, idx in edges_to_flow[j, i])))/capacity[i, j]) <= mlu for i, j in edges), "MLU CAP")
 
-    # Demand constraints (using first edge as representing flow of path)
+
+    # Demand constraints
     for s, d in paths:
-        model.addConstr(gp.quicksum(flow[s, d, paths[s, d][idx][0][0], paths[s, d][idx][0][1], idx] for idx in range(len(paths[s, d]))) <= demands[s, d], f"DEMAND_{s}_{d}")
+        model.addConstr(gp.quicksum(flow[s, d, idx] for idx in range(len(paths[s, d]))) <= demands[s, d], f"DEMAND_{s}_{d}")
 
-    # Non-zero demand constraints
-    for s, d in paths:
-        model.addConstr(gp.quicksum(flow[s, d, paths[s, d][idx][0][0], paths[s, d][idx][0][1], idx] for idx in range(len(paths[s, d]))) >= 0, f"MIN_FLOW_{s}_{d}")
-
-    model.setObjective((1 - mlu_weight) * gp.quicksum(flow[s, d, paths[s, d][idx][0][0], paths[s, d][idx][0][1], idx] for s, d, idx in iter_sd) - mlu_weight * mlu * total_demand, GRB.MAXIMIZE)
+    # Non-negative demand constraints
+    model.setObjective((1 - mlu_weight) * gp.quicksum(gp.quicksum(flow[s, d, idx] for idx in range(len(paths[s,d]))) for s, d in (paths)) - mlu_weight * mlu * total_demand, GRB.MAXIMIZE)
 
     model.optimize()
 
     # Below is code used for analysis
-    demand_met = defaultdict(int) # Maps source and destination to demand met
     edge_flow = defaultdict(int) # Maps edges to flow along those edges (bidirectional edges)
     total_demand_met = 0
-    seen = set() # To check if path has already been accounted for
 
-    # for v in model.getVars():
-    #     if v.X != 0:
-    #         if v.VarName != 'MLU':
-    #             o, s, d, a, b, i = (v.VarName).split('_')
-    #             if (s, d, i) not in seen:
-    #                 seen.add((s, d, i))
-    #                 demand_met[s, d] += v.X
-    #                 total_demand_met += v.X
-    #             if int(a) < int(b):
-    #                 edge_flow[a, b] += v.X
-    #             else:
-    #                 edge_flow[b, a] += v.X
-    #             # print('%s %g' % (v.VarName, v.X))
-    #         else:
-    #             print('%s %g' % (v.VarName, v.X))
-    #             print('%s %g' % (v.VarName, v.X * 1000000000))
+    
+    for v in model.getVars():
+        if v.X != 0:
+            if v.VarName != 'MLU':
+                _, s, d, idx = (v.VarName).split('_')
+                total_demand_met += v.X
+                for a, b in paths[int(s), int(d)][int(idx)]:
+                    if int(a) < int(b):
+                        edge_flow[a, b] += v.X
+                    else:
+                        edge_flow[b, a] += v.X
+                    # print('%s %g' % (v.VarName, v.X))
+            else:
+                print('%s %g' % (v.VarName, v.X))
+                print('%s %g' % (v.VarName, v.X * 1000000000))
             
-    # # Print flow through graph 
+    # Print flow through graph 
     # for x in demand_met:
     #     print (x,':',demand_met[x])
 
@@ -222,8 +201,8 @@ def draw_flow_allocations(graph, flow):
 def main(args):
     if len(args) > 1 and args[1] == 'extra':
         times = []
-        for i in range(1, 9):
-            size = i * 50
+        for i in range(1, 2):
+            size = i * 10
             graph = generate_graph(size)
             demands = generate_demands(size)
             model, flow = create_mcf_model(graph, demands)
@@ -240,9 +219,9 @@ def main(args):
         num_nodes = graph.number_of_nodes()
         demands = parse_demands(demands_file, num_nodes)
 
-        mlu_weight = 0
+        mlu_weight = 0.1
         mcf_model, flow = create_mcf_model(graph, demands, mlu_weight)
-        # draw_flow_allocations(graph, flow)
+    draw_flow_allocations(graph, flow)
 
 
 if __name__ == "__main__":
